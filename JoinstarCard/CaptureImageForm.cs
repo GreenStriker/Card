e1de74df68d16ab6974b9df26d92d4555c17810c
moukh rahman <moukhkst@hotmail.com>
2020-01-16 18:50:07 +0600

    #crop

--- JoinstarCard/CaptureImageForm.cs
+++ JoinstarCard/CaptureImageForm.cs
@@ -23,6 +23,8 @@ namespace JoinstarCard
             InitializeComponent();
 
             this.nw = now;
+
+            
         }
         FilterInfoCollection filterInfoCollection;
         VideoCaptureDevice captureDevice;
@@ -30,14 +32,20 @@ namespace JoinstarCard
 
         private void button2_Click(object sender, EventArgs e)
         {
-            nw.Visible = true;
+            pictureBox2.MouseDown += new MouseEventHandler(pictureBox2_MouseDown);
 
-            this.Visible = false;
+            pictureBox2.MouseMove += new MouseEventHandler(pictureBox2_MouseMove);
 
+            pictureBox2.MouseEnter += new EventHandler(pictureBox2_MouseEnter);
+            Controls.Add(pictureBox2);
 
 
 
         }
+
+
+
+
 
         private void CaptureImageForm_Load(object sender, EventArgs e)
         {
@@ -64,6 +72,78 @@ namespace JoinstarCard
         private void button1_Click(object sender, EventArgs e)
         {
             pictureBox2.Image = (Bitmap)pictureBox1.Image;
+        }
+        public Pen crpPen = new Pen(Color.White);
+        int crpX, crpY, rectW, rectH;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            //  label2.Text = "Dimensions :" + rectW + "," + rectH;
+            Cursor = Cursors.Default;
+            //Now we will draw the cropped image into pictureBox2
+            Bitmap bmp2 = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+            pictureBox2.DrawToBitmap(bmp2, pictureBox2.ClientRectangle);
+
+            Bitmap crpImg = new Bitmap(rectW, rectH);
+
+            for (int i = 0; i < rectW; i++)
+            {
+                for (int y = 0; y < rectH; y++)
+                {
+                    Color pxlclr = bmp2.GetPixel(crpX + i, crpY + y);
+                    crpImg.SetPixel(i, y, pxlclr);
+                }
+            }
+
+            pictureBox2.Image = (Image)crpImg;
+            pictureBox2.SizeMode = PictureBoxSizeMode.CenterImage;
+          
+           
+        }
+
+        private void pictureBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                Cursor = Cursors.Cross;
+                crpPen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+                // set initial x,y co ordinates for crop rectangle
+                //this is where we firstly click on image
+                crpX = e.X;
+                crpY = e.Y;
+
+            }
+        }
+
+        private void pictureBox2_MouseEnter(object sender, EventArgs e)
+        {
+            base.OnMouseEnter(e);
+            Cursor = Cursors.Cross;
+        }
+
+        private void pictureBox2_MouseMove(object sender, MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                pictureBox2.Refresh();
+                //set width and height for crop rectangle.
+                rectW = e.X - crpX;
+                rectH = e.Y - crpY;
+                Graphics g = pictureBox2.CreateGraphics();
+                g.DrawRectangle(crpPen, crpX, crpY, rectW, rectH);
+                g.Dispose();
+            }
+        }
+
+        protected override void OnMouseEnter(EventArgs e)
+        {
+            base.OnMouseEnter(e);
+            Cursor = Cursors.Default;
         }
     }
 }
